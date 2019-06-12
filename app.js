0fa28a9214990b6259ac3c4e484f1bd98899c01a
CN0071 <cn0071@CN0071.lan>
2019-06-12 17:34:47 +0100

    submitted but speed up moles doewsnbt work. need to target an element to change animation duration

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const holes = document.querySelectorAll('.holes');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mrbump');
-const molePeepInterval = 1000; //ms interval between peeping moles
+let molePeepInterval = 2000; //ms interval between peeping moles
 let peepIntervalTimer
 
 //bounce a random mole
 let bounceMole = () => {
+    console.log('bounceMole')
     let mole = ((Math.floor(Math.random() * 6 )))
+    //clear Mole classlist as if it fills up it stops - needs a delay
+    setTimeout( () => {moles[mole].classList.remove('bounce')}, 6000) //longer than animation duration?
     moles[mole].classList.add('bounce')   
     moles[mole].addEventListener("click", moleWhacked)
 }
@@ -20,12 +23,33 @@ let moleWhacked = () => {
     console.log(`score = ${score}`)
 }
 
-//MAIN - start game by clicking on Start Game button defined in HTML
+let speedUpMoles = () => {
+    if(score = Number(scoreBoard.innerHTML) >5) {
+        clearInterval(peepIntervalTimer)
+        bounce.animationDuration = "0.1s" //double speed of Moles peeping
+        peepIntervalTimer = setInterval(bounceMole, molePeepInterval)
+    } else { 
+        if(score = Number(scoreBoard.innerHTML) >10) { 
+            clearInterval(peepIntervalTimer)
+            bounce.animationDuration = "0.5s" //double speed of Moles peeping
+            peepIntervalTimer = setInterval(bounceMole, molePeepInterval)
+        } else {
+            if(score = Number(scoreBoard.innerHTML) >15) { 
+                clearInterval(peepIntervalTimer)
+                bounce.animationDuration = "0.1s" //double speed of Moles peeping
+                peepIntervalTimer = setInterval(bounceMole, molePeepInterval)
+            }
+        }
+    }
+}
+
+//MAIN - start/stop game by clicking on Start Game button defined in HTML
 let startGame = () => {
     if (document.getElementById("startButton").innerHTML == "START GAME") {
         console.log ('game started')
         //bounce a mole with an interval
         peepIntervalTimer = setInterval(bounceMole, molePeepInterval)
+        speedUpMoles()
         document.getElementById("startButton").innerHTML = "STOP GAME"
     } else {
         console.log ('game stopped')
