e26faba8ba6cdcbd4c94ad1813aa20dfe0387a0b
CN0071 <cn0071@CN0071.lan>
2019-06-12 16:09:10 +0100

    intermediate save of decent start

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-const holes = document.querySelectorAll('.hole');
+const holes = document.querySelectorAll('.holes');
 const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
+const moles = document.querySelectorAll('.mrbump');
+const molePeepInterval = 1000; //ms interval between peeping moles
+let peepIntervalTimer
+
+//bounce a random mole
+let bounceMole = () => {
+    let mole = ((Math.floor(Math.random() * 6 )))
+    moles[mole].classList.add('bounce')   
+    moles[mole].addEventListener("click", moleWhacked)
+}
+
+// detect a mole is whacked and add to score
+let moleWhacked = () => {
+    console.log(`mole whacked`)
+    let score = Number(scoreBoard.innerHTML)
+    score += 1
+    scoreBoard.innerHTML = score
+    console.log(`score = ${score}`)
+}
+
+//MAIN - start game by clicking on Start Game button defined in HTML
+let startGame = () => {
+    if (document.getElementById("startButton").innerHTML == "START GAME") {
+        console.log ('game started')
+        //bounce a mole with an interval
+        peepIntervalTimer = setInterval(bounceMole, molePeepInterval)
+        document.getElementById("startButton").innerHTML = "STOP GAME"
+    } else {
+        console.log ('game stopped')
+        //stop bouncing the moles
+        clearInterval(peepIntervalTimer)
+        document.getElementById("startButton").innerHTML = "START GAME"
+    }   
+}
